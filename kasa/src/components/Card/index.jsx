e9cf438d71e8611eb1a5d3d656ff6fc1f3a79f5b
dmc/dev <celsinodel13@gmail.com>
2024-03-14 19:23:06 +0100

    Last ajustements - caroussel et responsive

--- kasa/src/components/Card/index.jsx
+++ kasa/src/components/Card/index.jsx
@@ -7,6 +7,7 @@ function Card({ id, title, image }) {
   return (
     <div className="card-container">
       <Link to={`/ficheLogement/${id}`} className="card" style={{ backgroundImage: `url(${image})` }}>
+      <div className="gradient-overlay"></div>
         <h2 className="card-title">{title}</h2>
       </Link>
     </div>
--- kasa/src/pages/FicheLogement/index.jsx
+++ kasa/src/pages/FicheLogement/index.jsx
@@ -3,6 +3,8 @@ import { useParams, Navigate } from 'react-router-dom';
 import apartmentsData from '../../data/json P6.json'; 
 import Carousel from '../../components/Carousel'; 
 import Collapse from '../../components/Collapse'; 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 import '../../styles/fichelogement.scss'; 
 
 function FicheLogementPage() {
@@ -14,7 +16,25 @@ function FicheLogementPage() {
   }
 
   const { title, location, tags, description, equipments, rating, host, pictures } = apartment;
-  const starRating = Array.from({ length: 5 }, (_, index) => index < rating ? '★' : '☆').join(' ');
+
+function generateStarIcons(rating) {
+  let stars = [];
+  const fullStarsCount = Math.floor(rating); 
+  const emptyStarsCount = 5 - fullStarsCount; 
+
+  // Ajouter les étoiles pleines
+  for (let i = 0; i < fullStarsCount; i++) {
+    stars.push(<FontAwesomeIcon icon={faStar} color="#FFD700" className="custom-size" key={`full-${i}`} />);
+  }
+
+  // Ajouter les étoiles vides
+  for (let i = 0; i < emptyStarsCount; i++) {
+    stars.push(<FontAwesomeIcon icon={faStar} style={{ color: 'gray' }} className="custom-size" key={`empty-${i}`} />);
+  }
+
+  return stars;
+}
+
 
   return (
     <div className="fiche-logement">
@@ -35,7 +55,7 @@ function FicheLogementPage() {
               <img src={host.picture} alt="Host" className="host-picture" />
               <p className="host-name">{host.name}</p>
             </div>
-            <div className="rating">{starRating}</div>
+            <div className="rating">{generateStarIcons(rating)}</div>
           </div>
         </div>
         <div className="description-equipments-container">
