17f08939357ad4c669179cb405c805fa0e2c092a
dmc/dev <celsinodel13@gmail.com>
2024-03-24 22:39:05 +0100

    arrow icons caroussel

--- kasa/src/components/Carousel/index.jsx
+++ kasa/src/components/Carousel/index.jsx
@@ -1,30 +1,36 @@
 import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import '../../styles/carousel.scss';
 
 const Carousel = ({ pictures }) => {
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
     const totalImages = pictures.length;
 
-const nextImage = () => {
-    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % totalImages);
-};
+    const nextImage = () => {
+        setCurrentImageIndex((prevIndex) => (prevIndex + 1) % totalImages);
+    };
 
-const prevImage = () => {
-    setCurrentImageIndex((prevIndex) => (prevIndex - 1 + totalImages) % totalImages);
-};
+    const prevImage = () => {
+        setCurrentImageIndex((prevIndex) => (prevIndex - 1 + totalImages) % totalImages);
+    };
 
-return (
-    <div className="carousel">
-        {totalImages > 1 && (
-        <>
-            <button onClick={prevImage} className="carousel-button prev-button">{"<"}</button>
-            <button onClick={nextImage} className="carousel-button next-button">{">"}</button>
-            <div className="image-counter">{currentImageIndex + 1}/{totalImages}</div>
-        </>
-    )}
-    <img src={pictures[currentImageIndex]} alt={`Slide ${currentImageIndex}`} className="carousel-image"/>
-    </div>
-);
+    return (
+        <div className="carousel">
+            {totalImages > 1 && (
+                <>
+                    <button onClick={prevImage} className="carousel-button prev-button">
+                        <FontAwesomeIcon icon={faChevronLeft} />
+                    </button>
+                    <button onClick={nextImage} className="carousel-button next-button">
+                        <FontAwesomeIcon icon={faChevronRight} />
+                    </button>
+                    <div className="image-counter">{currentImageIndex + 1}/{totalImages}</div>
+                </>
+            )}
+            <img src={pictures[currentImageIndex]} alt={`Slide ${currentImageIndex}`} className="carousel-image"/>
+        </div>
+    );
 };
 
 export default Carousel;
